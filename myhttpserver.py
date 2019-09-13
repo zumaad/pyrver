4b007405f11627f8b2084ea3eca4f2b644ab6498
zumaad <khanzumaad@gmail.com>
2019-09-13 01:15:47 -0400

    adding handlers and managing handlers

--- myhttpserver.py
+++ myhttpserver.py
@@ -6,7 +6,17 @@ import selectors
 import datetime
 import logging
 from enum import Enum
+from http.server import BaseHTTPRequestHandler
+from io import BytesIO
+import json
+import os
+import pathlib
 
+"""
+design decisions i want the handlers to be in a class so that they are grouped together.
+I want a mapping of tasks to handlers so that i can just look up the task in the dictionary and return the
+appropriate handler instead of having a bunch of if statements. 
+"""
 
 logging.basicConfig(filename='server.log',
                             filemode='a',
@@ -22,15 +32,80 @@ PORT = int(args.port)
 client_manager = selectors.DefaultSelector()
 list_of_sockets = []
 
+
+
 class ClientInformation:
     def __init__(self,addr,socket_type):
         self.addr = addr
         self.socket_type = socket_type
 
+class HTTPRequest(BaseHTTPRequestHandler): 
+    """ This class subclasses BaseHTTPRequestHandler so that
+        i can take advantage of parse_request as i don't want to
+        write an http parser """
+
+    def __init__(self, request_text): 
+        self.rfile = BytesIO(request_text) #parse_request from parent class assumes this is set.
+        self.raw_requestline = self.rfile.readline() 
+        self.error_code = self.error_message = None 
+        self.parse_request() 
+
+    def send_error(self, code, message): 
+        self.error_code = code 
+        self.error_message = message 
+
 class SocketType(Enum):
     MASTER_SOCKET = 1
     CLIENT_SOCKET = 2
 
+
+class StaticAssetHandler:
+    def __init__(self, context):
+        self.static_directory_path = context
+        self.all_files = set(pathlib.Path(context).glob('**/*')) #get all files in the static directory
+
+    def static_asset_handler(self, path_to_static_file):
+        """ path_to_static_file is can be either just a filename or a path to a file in case
+            the requested asset is nested  
+        """
+        absolute_path = self.static_directory_path + path_to_static_file
+        
+        if pathlib.Path(absolute_path) in self.all_files:
+            static_file_contents = open(absolute_path,'rb').read()
+            return static_file_contents
+        else:
+            return b'file does not exist'
+
+
+        
+
+
+class ManageHandlers:
+    """
+    picks the handler based on settings and injects the needed context for each handler
+    """
+
+    def __init__(self, tasks_and_context):
+        self.tasks_and_context = tasks_and_context
+        self.task_to_handler = {'static':StaticAssetHandler}
+
+
+    def pick_handlers(self):
+        task_handlers = []
+        for task, context  in self.tasks_and_context.items():
+            if task in self.task_to_handler:
+                handlerClass = self.task_to_handler[task]
+                task_handlers.append(handlerClass(context))
+            else:
+                raise NotImplementedError
+        return task_handlers
+
+
+def settings_parser():
+    with open("settings.json",'r') as settings:
+        settings_dic = json.loads(settings.read())
+    return settings_dic
+    
 
 def log_debug_info(*args, stdout_print = False):
     str_args = [str(arg) for arg in args]
@@ -42,12 +117,10 @@ def log_debug_info(*args, stdout_print = False):
     else:
         logging.debug(logs)
 
-def http_parser(http_message):
-    # print(http_message.split('\n'))
-    print(http_message)
+
     
 
-def create_http_response(body):
+def create_http_response(body=None):
     if not body:
         length = 0
     else:
@@ -71,6 +144,7 @@ def accept_new_client(master_socket):
     client_manager.register(new_client_socket, selectors.EVENT_READ | selectors.EVENT_WRITE, data = ClientInformation(addr,SocketType.CLIENT_SOCKET))
     list_of_sockets.append(new_client_socket)
 
+
 def handle_client(socket_wrapper, events):
     recv_data = None 
     client_socket = socket_wrapper.fileobj
@@ -87,6 +161,7 @@ def handle_client(socket_wrapper, events):
         if not recv_data:
             close_client_connection(socket_wrapper)
         else:
+            client_socket.sendall(create_http_response())
             print(recv_data)
         
 def close_client_connection(socket_wrapper):
@@ -113,6 +188,8 @@ def server_loop():
                 handle_client(socket_wrapper, events)
 
 def main():
+    settings = settings_parser()
+    task_handlers = ManageHandlers(settings['tasks'])
     init_master_socket()
     server_loop()
 
