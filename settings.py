772b57c810e570ecf18a582d4c9d2fae76481c14
zumaad <khanzumaad@gmail.com>
2019-09-30 00:48:00 -0400

    finished settings for proxying

--- settings.py
+++ settings.py
@@ -1,11 +1,18 @@
 settings = {
     "tasks":{
-        "serveStatic":{
-            "matchCriteria": {"port":[str(port_num) for port_num in range(3330,3335)], 
-                              "host": ["testingserver.com","localhost","testingserver2.com"], 
-                              "url":["/static/"]
+        "serve_static":{
+            "match_criteria": {
+                "port":[str(port_num) for port_num in range(3330,3335)], 
+                "host": ["testingserver.com","localhost","testingserver.com2"], 
+                "url":["/static/"]
                               },
             "context": {"staticRoot":"/Users/zumaad/httpserver/static/"}
+        },
+        "proxy": {
+            "match_criteria": {
+                "host":["testingserver2.com"]
+                },
+            "context": {'send_to':['localhost:4000']}
         }
     }  
 }
