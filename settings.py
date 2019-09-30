a498f885186e927061650d2b56d40dc6c99af0a1
zumaad <khanzumaad@gmail.com>
2019-09-30 01:02:07 -0400

    Added comment about reverse_proxying vs load balancing in settings

--- settings.py
+++ settings.py
@@ -15,6 +15,18 @@ settings = {
                 "host":["testingserver2.com"]
                 },
             "context": {'send_to':'localhost:4000'}
+        },
+
+        "load_balance": {
+            "match_criteria": {
+                "host":["testingserver2.com"]
+                },
+            "context": {'send_to':'localhost:4000',"type":"round_robin"}
         }
     }  
 }
+#the diff between load_balance and reverse_proxy is that in reverse_proxy u can only specify one server as there is
+#no concept of reverse proxying to multiple servers at once. Furthermore, in load balancing u can specify types of load
+#balancing like round robin or weighted. 
+#i could make it such that if you specified multiple servers in reverse_proxy it assumes u want to load balance, but this
+#estabalishes a clearer distinction between the two.
